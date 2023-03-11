b3f60f21f89b3ff9d13a37db6809e57b0ab175e5
pablofrers93 <pablofrers93@gmail.com>
2023-03-11 18:48:09 -0300

    Custom markers

--- app/test/test_task.py
+++ app/test/test_task.py
@@ -7,10 +7,12 @@ from datetime import datetime
 from datetime import timedelta
 
 class TestTask():
-    
+     
+    @pytest.mark.news 
     def test_task(self):
         assert True
 
+    @pytest.mark.news
     def test_new_task(self):
         due_date = datetime.now() + timedelta(days = 1)
         task = Task('Title', 'Description', 'eduardo.gpg', due_date)
@@ -20,8 +22,16 @@ class TestTask():
         assert task.assigned_to == 'eduardo.gpg'
         assert task.due_date == due_date
         
-    
+    @pytest.mark.errors
+    @pytest.mark.due_date
     def test_due_date_error(self):
         with pytest.raises(DueDateError):
             due_date = datetime.now() - timedelta(days = 1)
             Task('Title', 'Description', 'eduardo.gpg', due_date)
+
+    @pytest.mark.due_date 
+    def test_due_date(self):
+        due_date = datetime.now() + timedelta(days = 1)
+        task = Task('Title', 'Description', 'eduardo.gpg', due_date)
+        
+        assert task.due_date > datetime.now()
