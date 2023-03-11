80c3f0ca91052ddc0e455372e7026e6eeb28c126
pablofrers93 <pablofrers93@gmail.com>
2023-03-11 18:10:43 -0300

    Crear nueva tarea

--- app/test/test_task.py
+++ app/test/test_task.py
@@ -1,6 +1,16 @@
 import pytest 
-
+from app.task import Task
+from datetime import datetime
 class TestTask():
     
     def test_task(self):
         assert True
+
+    def test_new_task(self):
+        due_date = datetime.now()
+        task = Task('Title', 'Description', 'eduardo.gpg', due_date)
+
+        assert task.title == 'Title'
+        assert task.description == 'Description'
+        assert task.assigned_to == 'eduardo.gpg'
+        assert task.due_date == due_date
