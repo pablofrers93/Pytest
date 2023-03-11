1e4950315ef495b0f3e81502e01ea6af52b2dc90
pablofrers93 <pablofrers93@gmail.com>
2023-03-11 17:22:42 -0300

    Pruebas unitarias agrupadas

--- test_main.py
+++ test_main.py
@@ -1,4 +1,9 @@
 import pytest
 
-def test_example():
-    assert 10 == 10
+class TestExample():
+    
+    def test_suma_dos_numeros(self):
+        assert 10 + 10 == 20, 'Lo sentimos, la suma no es correcta'
+    
+    def test_resta_dos_numeros(self):
+        assert 10 - 10 == 0, 'Lo sentimos, la resta no es correcta'
