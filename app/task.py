bacec459742922e92f5d6512a5500bfc1f6973d3
pablofrers93 <pablofrers93@gmail.com>
2023-03-11 22:42:35 -0300

    Coding cover

--- app/task.py
+++ app/task.py
@@ -3,13 +3,22 @@ from datetime import datetime
 
 class DueDateError(Exception):
     pass
+
+
 class Task():
 
-   def __init__(self, title, description, assigned_to, due_date):
-       self.title = title
-       self.description = description
-       self.assigned_to = assigned_to
-       self.due_date = due_date
-       
-       if due_date < datetime.now():
-           raise DueDateError('Lo sentimos, la fecha no es valida')
+    def __init__(self, title, description, assigned_to, due_date, status = 'PENDING'):
+        self.title = title
+        self.description = description
+
+        self.assigned_to = assigned_to
+        #self.status = status
+
+        if due_date < datetime.now():
+            raise DueDateError('Lo sentimos, la fecha no es valida')
+
+        self.due_date = due_date
+    def done(self):
+       self.status = 'DONE'
+    def undone(self):
+        self.status = 'PENDING'
